b7fac1767cb7e14d5afa2016e7cb3e5aabecb567
OleM <hamburgwind@posteo.de>
2022-04-01 16:26:41 +0200

    add test: calc and round tax :test_tube:

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+const taxRate = 10;
+const taxRateImported = 5;
+
+export function calcTax(price, exempt, imported) {
+  return 0;
+}
+
+export function roundTax(tax) {
+  return 0;
+}
+
 function App() {
   return (
     <div className="App">
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,33 @@
-test("test", () => {
-  expect(1).toEqual(1);
+import { calcTax, roundTax } from "./App";
+
+// --- calculating tax ---
+
+test("calc basic tax rate (10%)", () => {
+  // 1 music CD at 14.99 -> 1 music CD: 16.49
+  expect(calcTax(14.99)).toEqual(1.499);
+});
+
+test("calc import tax rate (5%)", () => {
+  // 1 imported box of chocolates at 10.00 -> 1 imported box of chocolates: 10.50
+  expect(calcTax(10, true, true)).toEqual(0.5);
+});
+
+test("calc basic tax rate (10%) + import tax rate (5%)", () => {
+  // 1 imported bottle of perfume at 47.50 -> 1 imported bottle of perfume: 54.65
+  expect(calcTax(47.5, false, true)).toEqual(7.125);
+});
+
+// --- rounding tax ---
+
+test("round tax according to the rounding rules", () => {
+  // 1 music CD at 14.99 -> 1 music CD: 16.49
+  expect(roundTax(1.499)).toEqual(1.5);
+  // 1 imported bottle of perfume at 47.50 -> 1 imported bottle of perfume: 54.65
+  expect(roundTax(7.125)).toEqual(7.15);
+  // 1 imported bottle of perfume at 27.99 -> 1 imported bottle of perfume: 32.19
+  expect(roundTax(4.198499999999999)).toEqual(4.2);
+  // 1 bottle of perfume at 18.99 -> 1 bottle of perfume: 20.89
+  expect(roundTax(1.8989999999999998)).toEqual(1.9);
+  // 1 box of imported chocolates at 11.25 -> 1 imported box of chocolates: 11.85
+  expect(roundTax(0.5625)).toEqual(0.6);
 });
