d3f9f8011fcacb606f93c78498187f8c3f053c58
OleM <hamburgwind@posteo.de>
2022-04-02 13:02:56 +0200

    add test: create receipt :test_tube:

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,22 @@ const taxRate = 10;
 const taxRateImported = 5;
 const taxFreeGoods = ["book", "chocolate", "pill"];
 
+const input1 = [
+  "1 book at 12.49",
+  "1 music CD at 14.99",
+  "1 chocolate bar at 0.85",
+];
+const input2 = [
+  "1 imported box of chocolates at 10.00",
+  "1 imported bottle of perfume at 47.50",
+];
+const input3 = [
+  "1 imported bottle of perfume at 27.99",
+  "1 bottle of perfume at 18.99",
+  "1 packet of headache pills at 9.75",
+  "1 box of imported chocolates at 11.25",
+];
+
 export function calcTax(price, exempt, imported) {
   const tax = exempt
     ? imported
@@ -38,6 +54,21 @@ export function convertInput(input) {
     import: imported,
     item: item,
     price: price,
+  };
+}
+
+export function createReceipt(inputs) {
+  return {
+    items: [
+      {
+        amount: 0,
+        import: false,
+        item: "none",
+        shelfPrice: 0,
+      },
+    ],
+    salesTaxes: 0,
+    total: 0,
   };
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,11 @@
 import { expect } from "vitest";
-import { calcTax, checkTaxExempt, convertInput, roundTax } from "./App";
+import {
+  calcTax,
+  checkTaxExempt,
+  convertInput,
+  createReceipt,
+  roundTax,
+} from "./App";
 
 // --- calculating tax ---
 
@@ -115,5 +121,103 @@ test("recognize input and convert accordingly", () => {
     import: true,
     item: "box of chocolates",
     price: 11.25,
+  });
+});
+
+// --- create receipt ---
+
+test("process input(s) and create receipt", () => {
+  // Input 1
+  expect(
+    createReceipt([
+      "1 book at 12.49",
+      "1 music CD at 14.99",
+      "1 chocolate bar at 0.85",
+    ])
+  ).toEqual({
+    items: [
+      {
+        amount: 1,
+        import: false,
+        item: "book",
+        shelfPrice: 12.49,
+      },
+      {
+        amount: 1,
+        import: false,
+        item: "music CD",
+        shelfPrice: 16.49,
+      },
+      {
+        amount: 1,
+        import: false,
+        item: "chocolate bar",
+        shelfPrice: 0.85,
+      },
+    ],
+    salesTaxes: 1.5,
+    total: 29.83,
+  });
+  // Input 2
+  expect(
+    createReceipt([
+      "1 imported box of chocolates at 10.00",
+      "1 imported bottle of perfume at 47.50",
+    ])
+  ).toEqual({
+    items: [
+      {
+        amount: 1,
+        import: true,
+        item: "box of chocolates",
+        shelfPrice: 10.5,
+      },
+      {
+        amount: 1,
+        import: true,
+        item: "bottle of perfume",
+        shelfPrice: 54.65,
+      },
+    ],
+    salesTaxes: 7.65,
+    total: 65.15,
+  });
+  // Input 3
+  expect(
+    createReceipt([
+      "1 imported bottle of perfume at 27.99",
+      "1 bottle of perfume at 18.99",
+      "1 packet of headache pills at 9.75",
+      "1 box of imported chocolates at 11.25",
+    ])
+  ).toEqual({
+    items: [
+      {
+        amount: 1,
+        import: true,
+        item: "bottle of perfume",
+        shelfPrice: 32.19,
+      },
+      {
+        amount: 1,
+        import: false,
+        item: "bottle of perfume",
+        shelfPrice: 20.89,
+      },
+      {
+        amount: 1,
+        import: false,
+        item: "packet of headache pills",
+        shelfPrice: 9.75,
+      },
+      {
+        amount: 1,
+        import: true,
+        item: "box of chocolates",
+        shelfPrice: 11.85,
+      },
+    ],
+    salesTaxes: 6.7,
+    total: 74.68,
   });
 });
